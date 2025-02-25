834ec31a02bd5f1548a4dcd45ea5b8b12f5928a5
Khousic <khousic.thammavarapu@kfintech.com>
2025-02-25 14:45:57 +0530

    Removed NEVPNManager.

--- SafeGuard/Classes/SGVPNConnection.m
+++ SafeGuard/Classes/SGVPNConnection.m
@@ -6,6 +6,8 @@
 //
 
 #import "SGVPNConnection.h"
+#import <ifaddrs.h>
+#import <arpa/inet.h>
 
 @implementation SGVPNConnection
 
@@ -17,21 +19,29 @@
     return self;
 }
 
+
 - (BOOL)isVPNConnected {
-    if (@available(iOS 9.0, *)) { // Ensure the code runs on supported versions
-        NEVPNManager *vpnManager = [NEVPNManager sharedManager];
-        NEVPNConnection *connection = vpnManager.connection;
-        
-        switch (connection.status) {
-            case NEVPNStatusConnected:
-                return YES; // VPN is connected
-            default:
-                return NO; // VPN is not connected
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    BOOL isVPNConnected = NO;
+
+    if (getifaddrs(&interfaces) == 0) {
+        temp_addr = interfaces;
+        while (temp_addr != NULL) {
+            NSString *interfaceName = [NSString stringWithUTF8String:temp_addr->ifa_name];
+
+            if ([interfaceName containsString:@"utun"] ||   // Used for VPN connections
+                [interfaceName containsString:@"ppp"] ||   // Used for PPTP VPN
+                [interfaceName containsString:@"ipsec"]) { // Used for IPSec VPN
+                isVPNConnected = YES;
+                break;
+            }
+            temp_addr = temp_addr->ifa_next;
         }
-    } else {
-        NSLog(@"VPN status check is not supported on this iOS version.");
-        return NO;
     }
+    freeifaddrs(interfaces);
+    return isVPNConnected;
 }
+
 
 @end
